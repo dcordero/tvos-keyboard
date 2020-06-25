765b709ae2ae51277f4dacc1fbd50ec8981ca0a5
David Cordero <david@corderoramirez.com>
2020-06-25 09:14:56 +0200

    Initial Commit

---
+++ TvOSKeyboardCollapses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TvOSKeyboardCollapses
+//
+//  Created by David Cordero on 25.06.20.
+//  Copyright © 2020 David Cordero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
